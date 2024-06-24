2fead9bf39e6dc3755016e5e3e0167bad301d4ed
BaiSeBase <19102664454@163.com>
2024-06-24 22:24:05 +0800

    修改

--- src/pages/trade/components/orderlist/index.tsx
+++ src/pages/trade/components/orderlist/index.tsx
@@ -19,8 +19,8 @@ export default function OrderList({ hyorders, nowTab }) {
     for (let index = 0; index < hyorders.length; index++) {
       const data = hyorders[index];
       const node = (
-        <li
-          className="hyjyjl-2"
+        <div
+          class="tradelistruning-2"
           onClick={() => {
             const sendData = {
               orderNo: data.orderNo,
@@ -39,64 +39,92 @@ export default function OrderList({ hyorders, nowTab }) {
             );
           }}
         >
-          <div className="hyjyjl-3">
-            <div className="hyjyjl-4">
-              <div className="hyjyjl-5">
-                {data.coinname}
-                <div className={data.hyzd == 1 ? "hyjyjl-32" : "hyjyjl-6"}>
-                  {data.hyzd == 1
-                    ? translate(getText("買多"))
-                    : translate(getText("買空"))}
-                </div>
-              </div>
-              <div className="hyjyjl-7">{data.buytime?.substring(0, 16)}</div>
-            </div>
-            <div className="hyjyjl-8">
-              <div className="hyjyjl-9">
-                <span className="hyjyjl-10">
-                  <span className="hyjyjl-11">
-                    {data.num.toFixed(2)}
-                    <span className="hyjyjl-12">
-                      <span className="hyjyjl-13">USDT</span>
-                    </span>
-                  </span>
+          <div class="tradelistruning-3">
+            <div class="tradelistruning-4">
+              <div class="tradelistruning-5">
+                <span class="tradelistruning-6">
+                  {data.status == 1 ? "委托中" : "已完成"}
                 </span>
               </div>
-              <div className="hyjyjl-14">
-                {translate(getText("盈虧"))}
-                {data.status != 1 && (
-                  <span className={data.isWin == 1 ? "hyjyjl-41" : "hyjyjl-15"}>
-                    <span className="hyjyjl-16">
-                      {data.isWin == 1 ? "+" : "-"}
-                      {data.ploss}
-                    </span>
-                  </span>
-                )}
-              </div>
+              {/* <div class="tradelistruning-7">撤单</div> */}
             </div>
-            <div className="hyjyjl-17"></div>
-            <div className="hyjyjl-18">
-              <div className="hyjyjl-19">
-                <div className="hyjyjl-20">
-                  {translate(getText("購買"))}
-                  {lan == "zh" ? "" : <span>&nbsp;</span>}
-                  {translate(getText("價格"))}
+            <div class="tradelistruning-8">
+              <div class="tradelistruning-9">
+                <div class="tradelistruning-10">
+                  <span class="tradelistruning-11">产品</span>
                 </div>
-                <div className="hyjyjl-21">{data.buyprice}</div>
+                <div class="tradelistruning-12"> {data.coinname}</div>
               </div>
-              <div className="hyjyjl-22">
-                <div className="hyjyjl-23">
-                  {translate(getText("結算價格"))}
+              <div class="tradelistruning-13">
+                <div class="tradelistruning-14">
+                  <span class="tradelistruning-15">方向</span>
                 </div>
-                <div className="hyjyjl-24">{data.sellprice}</div>
+                <div
+                  className={
+                    data.hyzd == 1
+                      ? "tradelistruning-16"
+                      : "tradelistruning-16-1"
+                  }
+                >
+                  {data.hyzd == 1
+                    ? translate(getText("看涨"))
+                    : translate(getText("看跌"))}
+                </div>
               </div>
-              <div className="hyjyjl-25">
-                <div className="hyjyjl-26">{translate(getText("周期"))}</div>
-                <div className="hyjyjl-27">{data.time}</div>
+              <div class="tradelistruning-17">
+                <div class="tradelistruning-18">
+                  <span class="tradelistruning-19">时间周期</span>
+                </div>
+                <div class="tradelistruning-20">{data.time}</div>
               </div>
+              <div class="tradelistruning-21">
+                <div class="tradelistruning-22">
+                  <span class="tradelistruning-23">数量</span>
+                </div>
+                <div class="tradelistruning-24"> {data.num.toFixed(2)}</div>
+              </div>
+              <div class="tradelistruning-25">
+                <div class="tradelistruning-26">
+                  <span class="tradelistruning-27">下单时间</span>
+                </div>
+                <div class="tradelistruning-28">
+                  {data.buytime?.substring(0, 16)}
+                </div>
+              </div>
+              <div class="tradelistruning-29">
+                <div class="tradelistruning-30">
+                  <span class="tradelistruning-31">开仓价格</span>
+                </div>
+                <div class="tradelistruning-32"> {data.buyprice}</div>
+              </div>
+              {data.status != 1 && (
+                <div class="tradelistruning-33">
+                  <div class="tradelistruning-34">
+                    <span class="tradelistruning-35">结算价格</span>
+                  </div>
+                  <div class="tradelistruning-36"> {data.sellprice}</div>
+                </div>
+              )}
+              <div class="tradelistruning-33">
+                <div class="tradelistruning-34">
+                  <span class="tradelistruning-35">成交量</span>
+                </div>
+                <div class="tradelistruning-36"> {data.num.toFixed(2)}</div>
+              </div>
+              {data.status != 1 && (
+                <div class="tradelistruning-33">
+                  <div class="tradelistruning-34">
+                    <span class="tradelistruning-35">亏盈</span>
+                  </div>
+                  <div class="tradelistruning-36">
+                    {data.isWin == 1 ? "+" : "-"}
+                    {data.ploss}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
-        </li>
+        </div>
       );
       nodes.push(node);
     }
@@ -116,9 +144,11 @@ export default function OrderList({ hyorders, nowTab }) {
     return nodes;
   };
   return (
-    <div style={{
-      borderTop: ".625rem solid #030708"
-    }}>
+    <div
+      style={{
+        borderTop: ".625rem solid #030708",
+      }}
+    >
       <div class="tradechangebar-1">
         <div class="tradechangebar-2">
           <div class="tradechangebar-3">
